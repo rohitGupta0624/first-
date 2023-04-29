66d9703cc3387c7e261cc9dbf0bbeaf33c12ab99
rohitGupta0624 <132051406+rohitGupta0624@users.noreply.github.com>
2023-04-29 14:13:45 +0530

    Add files via upload

---
+++ try.js
@@ -0,0 +1,7 @@
+console.log(items);
+var items = document.getElementsByClassName("list-group-item");
+console.log(items);
+items[2].style.backgroundColor = "green";
+for(var i=0; i<items.length; i++){
+    items[i].style.fontWeight = "bold";
+}
