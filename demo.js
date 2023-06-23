01439650ad591fc59f2a874633fb060bc8b10f63
rohitGupta0624 <132051406+rohitGupta0624@users.noreply.github.com>
2023-06-23 13:35:47 +0530

    Arrow function and Object

--- demo.js
+++ demo.js
@@ -1,1 +1,11 @@
-console.log('hello world');
+var a,b;
+var demo = (a,b) =>{
+    let product = a*b;
+    console.log(product); 
+}
+demo(3,4);
+let student = {
+   name : 'Rohit',
+   age : '21'
+}
+console.log(student);
