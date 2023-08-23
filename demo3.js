48247b44334b374cd28ac89ccc495ebddb0de633
rohitGupta0624 <132051406+rohitGupta0624@users.noreply.github.com>
2023-08-23 14:59:36 +0530

    body-parse

--- demo3.js
+++ demo3.js
@@ -1,12 +1,16 @@
 
 const ex = require('express');
+const bodyparser = require('body-parser');
 const app = ex();
-app.use((req,res,next) => {
-    console.log('this is middle');
-    next();
+app.use(bodyparser.urlencoded());
+app.use('/localhost',(req,res,next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="product"></input><input type="text" name="size"></input><button type="sumbit">Send</button></form>');
 });
-app.use((req,res,next) => {
-    console.log('this is another middleware');
-    res.send( { key1: value });
+app.post('/product',(req,res,next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+app.use('/',(req,res,next) => {
+    res.send('<h1>ERROR! go back</h1>');
 });
 app.listen(7001);
