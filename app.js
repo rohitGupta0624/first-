0a28c4627bfc2305667c18d945c83baa1b43b7e6
rohitGupta0624 <132051406+rohitGupta0624@users.noreply.github.com>
2023-10-16 17:24:07 +0530

    dynamicRoutes

--- app.js
+++ app.js
@@ -3,29 +3,22 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const app = express();
+const errorController = require('./controllers/error');
 
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-const contactUsRoutes = require('./routes/contact_us');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
-app.use(contactUsRoutes);
 
-app.use('/admin/success',(req,res,next) => {
-  res.sendFile(path.join(__dirname,'views','success.html'));
-})
-
-app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname,'views','404.html'));
-});
+app.use(errorController.get404);
 
 app.listen(3000);
